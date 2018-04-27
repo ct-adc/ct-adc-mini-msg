c13216da3998248bc893d5d6fedf9fbb1f69a1f0
skysunshine99 <skysunshine99@126.com>
2018-04-27 13:20:21 +0800

    update demo

--- src/js/app/main/app.vue
+++ src/js/app/main/app.vue
@@ -5,47 +5,56 @@
         <button type="button" class="btn btn-sm btn-danger" @click="error">失败</button>
         <button type="button" class="btn btn-sm btn-warning" @click="warn">警告</button>
         <button type="button" class="btn btn-sm btn-warning" @click="notVuePlugin">notVuePlugin</button>
+        <button type="button" class="btn btn-sm btn-warning" @click="destroy">destroy</button>
     </div>
 </template>
 <script>
     import MiniMsg from 'main';
 
     export default{
+        data(){
+            return {
+                currentMiniMsg: null
+            };
+        },
         methods: {
             info(){
-                this.$minimsg({
+                this.currentMiniMsg = this.$minimsg({
                     content: '操作提示，将为您刷新页面...',
                     type: 'info',
                     duration: 1
                 });
             },
             success(){
-                this.$minimsg({
+                this.currentMiniMsg = this.$minimsg({
                     content: '操作成功，将为您刷新页面...',
                     type: 'success',
                     duration: 2
                 });
             },
             error(){
-                this.$minimsg({
+                this.currentMiniMsg = this.$minimsg({
                     content: '操作失败，将为您刷新页面...',
                     type: 'error',
                     duration: 2
                 });
             },
             warn(){
-                this.$minimsg({
+                this.currentMiniMsg = this.$minimsg({
                     content: '操作警告，将为您刷新页面...',
                     type: 'warning',
                     duration: 2
                 });
             },
             notVuePlugin(){
-                new MiniMsg({
+                this.currentMiniMsg = new MiniMsg({
                     content: 'notVuePlugin操作成功，将为您刷新页面...',
                     type: 'success',
                     duration: 2
                 }).animation();
+            },
+            destroy(){
+                this.currentMiniMsg.destroy();
             }
         }
     };
